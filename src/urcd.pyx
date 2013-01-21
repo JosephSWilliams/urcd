621a0a4a76b84b53895e5910b821d4d80beb6eff
root <root@d3v11.ano>
2013-01-21 05:24:00 +0000

    [urcd] workaround test for mirc

--- src/urcd.pyx
+++ src/urcd.pyx
@@ -106,8 +106,8 @@ while 1:
           continue
 
         os.write(wr,
-          ':'+serv+' NOTICE * :IGNORING HOSTNAME\n'
-          ':'+serv+' NOTICE * :IGNORING HOST RESOLUTION\n'
+          ':'+serv+' NOTICE AUTH :*** IGNORING HOSTNAME\n'
+          ':'+serv+' NOTICE AUTH :*** IGNORING HOST RESOLUTION\n'
           ':'+serv+' 001 '+nick+' :'+serv+'\n'
           ':'+serv+' 002 '+nick+' :'+nick+'!'+user+'@'+serv+'\n'
           ':'+serv+' 003 '+nick+' :'+serv+'\n'
