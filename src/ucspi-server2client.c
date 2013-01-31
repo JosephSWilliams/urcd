05b1832bbc96c451df1dc9d9aa46a721989d2eff
root <root@d3v11.ano>
2013-01-31 17:34:38 +0000

    [urcd] added ucspi-server2client.c

---
+++ src/ucspi-server2client.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+#define USAGE "Usage: ucspi-server2client prog [args]\n"
+int main (int argc, char **argv) {
+ if (argc>2) {
+  dup2(0,6);
+  dup2(1,7);
+  execvp(argv[1],argv+1);
+ }
+ write(2,USAGE,strlen(USAGE));
+ exit(255);
+}
