de62a827279daa5d074e47e63c39bf0d41d36606
root <root@d3v11.ano>
2013-02-11 06:55:39 +0000

    [src/ucspi-socks4aclient.c] check more careful argv, also less lines

--- src/ucspi-socks4aclient.c
+++ src/ucspi-socks4aclient.c
@@ -1,37 +1,25 @@
 #define USAGE "Usage: ucspi-socks4aclient addr port prog [args]\n"
 int main(int argc, char **argv){
 
-if ((argc<4)|(strlen(argv[1])>256)|(atoi(argv[2])>65535)){
+if ((argc<4)||(strlen(argv[1])>256)||(atoi(argv[2])<0)||(atoi(argv[2])>65535)){
   write(2,USAGE,strlen(USAGE));
   exit(64);}
 
-unsigned char packet[512] = {0};
+unsigned char packet[512]={0};
 packet[ 0] = '\x04';
 packet[ 1] = '\x01';
-packet[ 2] = atoi(argv[2]) / 256;
-packet[ 3] = atoi(argv[2]) % 256;
-packet[ 4] = '\x00';
-packet[ 5] = '\x00';
-packet[ 6] = '\x00';
+packet[ 2] = atoi(argv[2])/256;
+packet[ 3] = atoi(argv[2])%256;
 packet[ 7] = '\x01';
 packet[ 8] = 'u';
 packet[ 9] = 'c';
 packet[10] = 's';
 packet[11] = 'p';
 packet[12] = 'i';
-packet[13] = '\x00';
 
 memmove(&packet[14],argv[1],strlen(argv[1])+1);
-
-if (write(7,packet,14+strlen(argv[1])+1)<14+strlen(argv[1])+1)
-  exit(128+111);
-
+if (write(7,packet,14+strlen(argv[1])+1)<14+strlen(argv[1])+1) exit(128+111);
 bzero(packet,512);
-
-if (read(6,packet,8)<8)
-  exit(128+32);
-
-if ((packet[0]!=0)|(packet[1]!=90))
-  exit(128+111);
-
+if (read(6,packet,8)<8) exit(128+32);
+if ((packet[0]!=0)||(packet[1]!=90)) exit(128+111);
 execvp(argv[3],argv+3);}
