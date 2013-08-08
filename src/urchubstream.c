42fe12000211b3764d88f54f185cc15559387ae1
root <root@ip-10-56-75-16.(none)>
2013-08-08 17:46:05 +0000

    [urchubstream.c] sd for socket fd (thanks hay)

--- src/urchubstream.c
+++ src/urchubstream.c
@@ -34,7 +34,7 @@ main(int argc, char **argv)
     exit(64);
   }
 
-  int rd = 0, wr = 1;
+  int rd = 0, wr = 1, sd = -1;
   if (getenv("TCPCLIENT")){ rd = 6; wr = 7; }
 
   char user[UNIX_PATH_MAX] = {0};
@@ -54,19 +54,19 @@ main(int argc, char **argv)
     exit(signum);
   } signal(SIGINT,sock_close); signal(SIGHUP,sock_close); signal(SIGTERM,sock_close); 
 
-  if (socket(AF_UNIX,SOCK_DGRAM,0)!=3) exit(2);
+  if ((sd=socket(AF_UNIX,SOCK_DGRAM,0))<0) exit(2);
   int n = 1;
-  if (setsockopt(3,SOL_SOCKET,SO_REUSEADDR,&n,sizeof(n))<0) exit(3);
+  if (setsockopt(sd,SOL_SOCKET,SO_REUSEADDR,&n,sizeof(n))<0) exit(3);
   int userlen = strlen(user);
   if (userlen > UNIX_PATH_MAX) exit(4);
   memcpy(&sock.sun_path,user,userlen+1);
   unlink(sock.sun_path);
-  if (bind(3,(struct sockaddr *)&sock,sizeof(sock.sun_family)+userlen)<0) exit(5);
-  if (fcntl(3,F_SETFL,O_NONBLOCK)<0) sock_close(6);
+  if (bind(sd,(struct sockaddr *)&sock,sizeof(sock.sun_family)+userlen)<0) exit(5);
+  if (fcntl(sd,F_SETFL,O_NONBLOCK)<0) sock_close(6);
 
   struct pollfd fds[2];
   fds[0].fd = rd; fds[0].events = POLLIN | POLLPRI;
-  fds[1].fd = 3; fds[1].events = POLLIN;
+  fds[1].fd = sd; fds[1].events = POLLIN;
 
   struct sockaddr_un hub;
   bzero(&hub,sizeof(hub));
@@ -95,13 +95,13 @@ main(int argc, char **argv)
         i = read(rd,buffer+n,l-n);
         if (i<1) sock_close(9);
         n += i;
-      } if (sendto(3,buffer,n,0,(struct sockaddr *)&hub,sizeof(hub))<0) usleep(250000);
+      } if (sendto(sd,buffer,n,0,(struct sockaddr *)&hub,sizeof(hub))<0) usleep(250000);
 
     }
 
     while (poll(fds+1,1,0))
     {
-      n = read(3,buffer,2+16+8+1024);
+      n = read(sd,buffer,2+16+8+1024);
       if (n<1) sock_close(10);
       if (n!=2+16+8+buffer[0]*256+buffer[1]) continue;
       if (write(wr,buffer,n)<0) sock_close(11);
