8e1ec5a9deb454144fbe621a0e613f924e956216
root <root@ip-10-156-95-218.(none)>
2013-08-17 01:42:37 +0000

    [urcd.pyx][security] eliminate some side channels

--- src/urcd.pyx
+++ src/urcd.pyx
@@ -160,7 +160,8 @@ def try_write(fd,buffer):
 
 if URCHUB:
   def randombytes(n): return ''.join(choice(bytes)[0] for i in xrange(0,n))
-  def taia_now(): return { 'sec':4611686018427387914L+long(now),'nano':long(1000000000*(now%1)+500),'atto':0 }
+  ### version of taia_now is randomized by +/- 4 seconds ###
+  def taia_now(): return { 'sec':4611686018427387914L+long(now+choice([-1,-2,-3,-4,1,2,3,4])),'nano':long(1000000000*(now%1)+500),'atto':0 }
   def tai_pack(s): return chr(s['sec']>>56&255)+chr(s['sec']>>48&255)+chr(s['sec']>>40&255)+chr(s['sec']>>32&255)+chr(s['sec']>>24&255)+chr(s['sec']>>16&255)+chr(s['sec']>>8&255)+chr(s['sec']&255)
   def taia_pack(s): return tai_pack(s)+chr(s['nano']>>24&255)+chr(s['nano']>>16&255)+chr(s['nano']>>8&255)+chr(s['nano']&255)+chr(s['atto']>>24&255)+chr(s['atto']>>16&255)+chr(s['atto']>>8&255)+chr(s['atto']&255)
   def sock_write(buffer):
