f939643b842b06f8834292aa698b3e39b84c044c
root <root@administrator.(none)>
2014-02-02 21:35:22 +0000

    [src/urcd.pyx] antiflood vs limit fix. use round() instead of int()

--- src/urcd.pyx
+++ src/urcd.pyx
@@ -357,7 +357,7 @@ while 1:
 
     elif re_CLIENT_PRIVMSG_NOTICE_TOPIC_PART(buffer):
       if FLOOD:
-        flood += 1 + int(LIMIT)
+        flood += round(1.0 + LIMIT)
         if flood >= FLOOD:
           try_write(wr,':'+serv+' NOTICE '+Nick+' :RPL_SPAM\n')
           continue
