f659ba0d981f6f30d9fd6f4bfddd91cb88633070
root <root@localhost.(none)>
2014-07-30 07:00:39 +0000

    [Make.sh, ./bin/add-urchub] touch-ups ; set `ulimit -s 8192' for operating systems with an insufficient default (thanks epoch)

--- Make.sh
+++ Make.sh
@@ -77,24 +77,23 @@ fi
 
 if ! which cython 2>/dev/null ; then
  cp $src/urcd.pyx urcd || exit 1
- chmod +x urcd        || exit 1
+ chmod +x urcd || exit 1
  cp $src/urc2sd.pyx urc2sd || exit 1
- chmod +x urc2sd          || exit 1
+ chmod +x urc2sd || exit 1
  cp $src/taia96n.pyx taia96n.py || exit 1
-
  rm -rf libsodium_src
  exit 0 
 fi
 
 mkdir -p build || exit 1
 
-cython --embed $src/urcd.pyx -o build/urcd.c         || exit 1
+cython --embed $src/urcd.pyx -o build/urcd.c || exit 1
 gcc `cat conf-cc` -O2 -c build/urcd.c -I $PYTHON_HEADERS -o build/urcd.o || exit 1
-gcc `cat conf-cc` -O1 -o urcd build/urcd.o -l python2.7           || exit 1
+gcc `cat conf-cc` -O1 -o urcd build/urcd.o -l python2.7 || exit 1
 
-cython --embed $src/urc2sd.pyx -o build/urc2sd.c         || exit 1
+cython --embed $src/urc2sd.pyx -o build/urc2sd.c || exit 1
 gcc `cat conf-cc` -O2 -c build/urc2sd.c -I $PYTHON_HEADERS -o build/urc2sd.o || exit 1
-gcc `cat conf-cc` -O1 -o urc2sd build/urc2sd.o -l python2.7           || exit 1
+gcc `cat conf-cc` -O1 -o urc2sd build/urc2sd.o -l python2.7 || exit 1
 
 cython $src/taia96n.pyx -o build/taia96n.c || exit 1
 gcc `cat conf-cc` -O2 -shared -pthread -fPIC -fwrapv -Wall \
