b48e1206f0e44349aaedfc90ef7ccb2305a30741
root <root@localhost.(none)>
2014-10-05 08:32:57 +0000

    [liburc] urcsecretbox_fmt()

--- src/liburc.c
+++ src/liburc.c
@@ -10,55 +10,83 @@ exit(255);
 #endif
 
 PyObject *pyurchub_fmt(PyObject *self, PyObject *args, PyObject *kw) {
-  unsigned char p[2+12+4+8+1024];
-  char *b;
-  Py_ssize_t bsize = 0;
-  static const char *kwlist[] = {"b",0};
-  if (!PyArg_ParseTupleAndKeywords(
-   args,
-   kw,
-   "|s#:urchub_fmt",
-   (char **)kwlist,
-   &b,
-   &bsize
-  )) return Py_BuildValue("i", -1);
-  bsize &= 1023; /* security: prevent overflow */
-  if (urchub_fmt(p,b,bsize) == -1) return Py_BuildValue("i", -1);
-  return PyBytes_FromStringAndSize((char *)p, 2+12+4+8+bsize);
+ unsigned char p[2+12+4+8+1024];
+ char *b;
+ Py_ssize_t bsize = 0;
+ static const char *kwlist[] = {"b",0};
+ if (!PyArg_ParseTupleAndKeywords(
+  args,
+  kw,
+  "|s#:urchub_fmt",
+  (char **)kwlist,
+  &b,
+  &bsize
+ )) return Py_BuildValue("i", -1);
+ bsize &= 1023; /* security: prevent overflow */
+ if (urchub_fmt(p,b,bsize) == -1) return Py_BuildValue("i", -1);
+ return PyBytes_FromStringAndSize((char *)p, 2+12+4+8+bsize);
 }
 
 PyObject *pyurcsign_fmt(PyObject *self, PyObject *args, PyObject *kw) {
-  unsigned char p[2+12+4+8+1024+64];
-  char *b;
-  char *sk;
-  Py_ssize_t bsize = 0;
-  Py_ssize_t sksize = 0;
-  static const char *kwlist[] = {"b","sk",0};
-  if ((!PyArg_ParseTupleAndKeywords(
-   args,
-   kw,
-   "|s#s#:urcsign_fmt",
-   (char **)kwlist,
-   &b,
-   &bsize,
-   &sk,
-   &sksize
-  )) || (sksize != 64)) return Py_BuildValue("i", -1);
-  bsize &= 1023; /* security: prevent overflow */
-  if (urcsign_fmt(p,b,bsize,sk) == -1) return Py_BuildValue("i", -1);
-  return PyBytes_FromStringAndSize((char *)p, 2+12+4+8+bsize+64);
+ unsigned char p[2+12+4+8+1024+64];
+ char *b;
+ char *sk;
+ Py_ssize_t bsize = 0;
+ Py_ssize_t sksize = 0;
+ static const char *kwlist[] = {"b","sk",0};
+ if ((!PyArg_ParseTupleAndKeywords(
+  args,
+  kw,
+  "|s#s#:urcsign_fmt",
+  (char **)kwlist,
+  &b,
+  &bsize,
+  &sk,
+  &sksize
+ )) || (sksize != 64)) return Py_BuildValue("i", -1);
+ bsize &= 1023; /* security: prevent overflow */
+ if (urcsign_fmt(p,b,bsize,sk) == -1) return Py_BuildValue("i", -1);
+ return PyBytes_FromStringAndSize((char *)p, 2+12+4+8+bsize+64);
+}
+
+PyObject *pyurcsecretbox_fmt(PyObject *self, PyObject *args, PyObject *kw) {
+ unsigned char p[2+12+4+8+1024+16]; 
+ unsigned char m[1024*2];
+ unsigned char c[1024*2];
+ char *b;
+ char *sk;
+ Py_ssize_t bsize = 0;
+ Py_ssize_t sksize = 0;
+ static const char *kwlist[] = {"b","sk",0};
+ bzero(m,32); /* http://nacl.cr.yp.to/secretbox.html */
+ bzero(c,16);
+ if ((!PyArg_ParseTupleAndKeywords(
+  args,
+  kw,
+  "|s#s#:urcsign_fmt",
+  (char **)kwlist,
+  &b,
+  &bsize,
+  &sk,
+  &sksize
+ )) || (sksize != 32)) return Py_BuildValue("i", -1);
+ bsize &= 1023; /* security: prevent overflow */
+ if (urcsecretbox_fmt(p,b,bsize,sk) == -1) return Py_BuildValue("i", -1);
+ return PyBytes_FromStringAndSize((char *)p, 2+12+4+8+bsize+16);
 }
 
 /* hack __init__ */
 PyObject *pyliburc(PyObject *self) { return Py_BuildValue("i", -1); }
 
 static PyMethodDef Module_methods[] = {
- { "liburc",      pyliburc,      METH_NOARGS },
- { "urchub_fmt",  pyurchub_fmt,  METH_VARARGS|METH_KEYWORDS},
- { "urcsign_fmt", pyurcsign_fmt, METH_VARARGS|METH_KEYWORDS},
+ { "liburc",           pyliburc,           METH_NOARGS },
+ { "urchub_fmt",       pyurchub_fmt,       METH_VARARGS|METH_KEYWORDS},
+ { "urcsign_fmt",      pyurcsign_fmt,      METH_VARARGS|METH_KEYWORDS},
+ { "urcsecretbox_fmt", pyurcsecretbox_fmt, METH_VARARGS|METH_KEYWORDS},
  { NULL, NULL}
 };
 
-void initliburc()      { (void) Py_InitModule("liburc",      Module_methods); }
-void initurchub_fmt()  { (void) Py_InitModule("urchub_fmt",  Module_methods); }
-void initurcsign_fmt() { (void) Py_InitModule("urcsign_fmt", Module_methods); }
+void initliburc(){ (void) Py_InitModule("liburc", Module_methods); }
+void initurchub_fmt(){ (void) Py_InitModule("urchub_fmt", Module_methods); }
+void initurcsign_fmt(){ (void) Py_InitModule("urcsign_fmt", Module_methods); }
+void initurcsecretbox_fmt(){ (void) Py_InitModule("urcsecretbox_fmt", Module_methods); }
--- src/liburc.h
+++ src/liburc.h
@@ -4,11 +4,18 @@
 #include <nacl/crypto_sign.h>
 #include <nacl/crypto_box.h>
 #include <sys/time.h>
+#include <strings.h>
 #include <stdlib.h>
 #include <fcntl.h>
 #include <tai.h>
 
 /* security: enforce compatibility and santize malicious configurations */
+#if crypto_secretbox_BOXZEROBYTES != 16
+exit(255);
+#endif
+#if crypto_secretbox_ZEROBYTES != 32
+exit(255);
+#endif
 #if crypto_sign_BYTES != 64
 exit(255);
 #endif
@@ -68,5 +75,24 @@ int urcsign_fmt(unsigned char *p, unsigned char *b, int blen, unsigned char *sk)
  if (crypto_sign(sm,&smlen,p,2+12+4+8+blen,sk) == -1) return -1;
  memmove(p+2+12+4+8+blen,sm,32);
  memmove(p+2+12+4+8+blen+32,sm+32+blen,32);
+ return 0;
+}
+
+int urcsecretbox_fmt(unsigned char *p, unsigned char *b, int blen, unsigned char *sk) {
+ unsigned char m[1024*2];
+ unsigned char c[1024*2];
+ bzero(m,32); /* http://nacl.cr.yp.to/secretbox.html */
+ bzero(c,16);
+ blen &= 1023; /* security: prevent overflow */
+ setlen(p,blen+16);
+ taia96n(p+2);
+ p[12]=2;
+ p[13]=0;
+ p[14]=0;
+ p[15]=0;
+ randombytes(p+2+12+4,8);
+ memmove(m+32,b,blen);
+ if (crypto_secretbox(c,m,32+blen,(const unsigned char *)p+2,(const unsigned char *)sk) == -1) return -1;
+ memmove(p+2+12+4+8,c+16,blen+16);
  return 0;
 }
