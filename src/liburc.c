bb7a0d38c22d841bcd1c1131dfa129bd5d9e7b06
root <root@localhost.(none)>
2014-11-16 19:44:01 +0000

    [liburc] urccryptobox functions

--- src/liburc.c
+++ src/liburc.c
@@ -12,12 +12,11 @@ exit(255);
 exit(255);
 #endif
 
-#define URC_MTU 2+12+4+8+1024
-#define URC_MTU_MASK 1023
-#define IRC_MTU_MASK 511
+#define URC_MTU 1024
+#define IRC_MTU 512
 
 PyObject *pyurchub_fmt(PyObject *self, PyObject *args, PyObject *kw) {
- unsigned char p[2+12+4+8+1024];
+ unsigned char p[1024];
  char *b;
  Py_ssize_t bsize = 0;
  static const char *kwlist[] = {"b",0};
@@ -29,19 +28,19 @@ PyObject *pyurchub_fmt(PyObject *self, PyObject *args, PyObject *kw) {
   &b,
   &bsize
  )) return Py_BuildValue("i", -1);
- if (bsize > IRC_MTU_MASK) return Py_BuildValue("i", -1);
+ if (bsize > IRC_MTU) return Py_BuildValue("i", -1);
  if (urchub_fmt(p,b,bsize) == -1) return Py_BuildValue("i", -1);
  return PyBytes_FromStringAndSize((char *)p, 2+12+4+8+bsize);
 }
 
 PyObject *pyurcsign_fmt(PyObject *self, PyObject *args, PyObject *kw) {
- unsigned char p[2+12+4+8+1024+64];
+ unsigned char p[1024];
  char *b;
  char *sk;
  Py_ssize_t bsize = 0;
  Py_ssize_t sksize = 0;
  static const char *kwlist[] = {"b","sk",0};
- if ((!PyArg_ParseTupleAndKeywords(
+ if (!PyArg_ParseTupleAndKeywords(
   args,
   kw,
   "|s#s#:urcsign_fmt",
@@ -50,17 +49,19 @@ PyObject *pyurcsign_fmt(PyObject *self, PyObject *args, PyObject *kw) {
   &bsize,
   &sk,
   &sksize
- )) || (sksize != 64)) return Py_BuildValue("i", -1);
- if (bsize > IRC_MTU_MASK) return Py_BuildValue("i", -1);
+ )) return Py_BuildValue("i", -1);
+ if (sksize != 64) return Py_BuildValue("i", -1);
+ if (bsize > IRC_MTU) return Py_BuildValue("i", -1);
  if (urcsign_fmt(p,b,bsize,sk) == -1) return Py_BuildValue("i", -1);
  return PyBytes_FromStringAndSize((char *)p, 2+12+4+8+bsize+64);
 }
 
 PyObject *pyurcsign_verify(PyObject *self, PyObject *args, PyObject *kw) {
- static const char *kwlist[] = {"p", "pk", 0};
- unsigned char *p, *pk;
+ unsigned char *p;
+ unsigned char *pk;
  Py_ssize_t psize=0, pksize=0;
- if ((!PyArg_ParseTupleAndKeywords(
+ static const char *kwlist[] = {"p", "pk", 0};
+ if (!PyArg_ParseTupleAndKeywords(
   args,
   kw,
   "|s#s#:urcsign_verify",
@@ -69,19 +70,20 @@ PyObject *pyurcsign_verify(PyObject *self, PyObject *args, PyObject *kw) {
   &psize,
   (char **)&pk,
   &pksize
- )) || (pksize != 32)) return Py_BuildValue("i", -1);
+ )) return Py_BuildValue("i", -1);
+ if (pksize != 32) return Py_BuildValue("i", -1);
  if (urcsign_verify(p,psize,pk) == -1) return Py_BuildValue("i", -1);
  return Py_BuildValue("i", 0);
 }
 
 PyObject *pyurcsecretbox_fmt(PyObject *self, PyObject *args, PyObject *kw) {
- unsigned char p[2+12+4+8+1024+16];
+ unsigned char p[1024];
  char *b;
  char *sk;
  Py_ssize_t bsize = 0;
  Py_ssize_t sksize = 0;
  static const char *kwlist[] = {"b","sk",0};
- if ((!PyArg_ParseTupleAndKeywords(
+ if (!PyArg_ParseTupleAndKeywords(
   args,
   kw,
   "|s#s#:urcsecretbox_fmt",
@@ -90,8 +92,9 @@ PyObject *pyurcsecretbox_fmt(PyObject *self, PyObject *args, PyObject *kw) {
   &bsize,
   &sk,
   &sksize
- )) || (sksize != 32)) return Py_BuildValue("i", -1);
- if (bsize > IRC_MTU_MASK) return Py_BuildValue("i", -1);
+ )) return Py_BuildValue("i", -1);
+ if (sksize != 32) return Py_BuildValue("i", -1);
+ if (bsize > IRC_MTU) return Py_BuildValue("i", -1);
  if (urcsecretbox_fmt(p,b,bsize,sk) == -1) return Py_BuildValue("i", -1);
  return PyBytes_FromStringAndSize((char *)p, 2+12+4+8+bsize+16);
 }
@@ -103,7 +106,7 @@ PyObject *pyurcsecretbox_open(PyObject *self, PyObject *args, PyObject *kw) {
  Py_ssize_t psize = 0;
  Py_ssize_t sksize = 0;
  static const char *kwlist[] = {"p","sk",0};
- if ((!PyArg_ParseTupleAndKeywords(
+ if (!PyArg_ParseTupleAndKeywords(
   args,
   kw,
   "|s#s#:urcsecretbox_open",
@@ -112,9 +115,66 @@ PyObject *pyurcsecretbox_open(PyObject *self, PyObject *args, PyObject *kw) {
   &psize,
   &sk,
   &sksize
- )) || (sksize != 32)) return Py_BuildValue("i", -1);
+ )) return Py_BuildValue("i", -1);
+ if (sksize != 32) return Py_BuildValue("i", -1);
  if (psize > URC_MTU) return Py_BuildValue("i", -1);
  if (urcsecretbox_open(b,p,psize,sk) == -1) return Py_BuildValue("i", -1);
+ return PyBytes_FromStringAndSize((char *)b, -2-12-4-8+psize-16);
+}
+
+PyObject *pyurccryptobox_fmt(PyObject *self, PyObject *args, PyObject *kw) {
+ unsigned char p[1024];
+ char *b;
+ char *pk;
+ char *sk;
+ Py_ssize_t bsize = 0;
+ Py_ssize_t pksize = 0;
+ Py_ssize_t sksize = 0;
+ static const char *kwlist[] = {"b","pk","sk",0};
+ if (!PyArg_ParseTupleAndKeywords(
+  args,
+  kw,
+  "|s#s#s#:urccryptobox_fmt",
+  (char **)kwlist,
+  &b,
+  &bsize,
+  &pk,
+  &pksize,
+  &sk,
+  &sksize
+ )) return Py_BuildValue("i", -1);
+ if (pksize != 32) return Py_BuildValue("i", -1);
+ if (sksize != 32) return Py_BuildValue("i", -1);
+ if (bsize > IRC_MTU) return Py_BuildValue("i", -1);
+ if (urccryptobox_fmt(p,b,bsize,pk,sk) == -1) return Py_BuildValue("i", -1);
+ return PyBytes_FromStringAndSize((char *)p, 2+12+4+8+bsize+16);
+}
+
+PyObject *pyurccryptobox_open(PyObject *self, PyObject *args, PyObject *kw) {
+ unsigned char b[1024];
+ char *p;
+ char *pk;
+ char *sk;
+ Py_ssize_t psize = 0;
+ Py_ssize_t pksize = 0;
+ Py_ssize_t sksize = 0;
+ static const char *kwlist[] = {"p","pk","sk",0};
+ if (!PyArg_ParseTupleAndKeywords(
+  args,
+  kw,
+  "|s#s#s#:urccryptobox_open",
+  (char **)kwlist,
+  &p,
+  &psize,
+  &pk,
+  &pksize,
+  &sk,
+  &sksize
+ )) return Py_BuildValue("i", -1);
+ if (pksize != 32) return Py_BuildValue("i", -1);
+ if (sksize != 32) return Py_BuildValue("i", -1);
+ if (psize > URC_MTU) return Py_BuildValue("i", -1);
+ if (urccryptobox_open(b,p,psize,pk,sk) == -1) return Py_BuildValue("i", -1);
  return PyBytes_FromStringAndSize((char *)b, -2-12-4-8+psize-16);
 }
 
@@ -122,12 +182,14 @@ PyObject *pyurcsecretbox_open(PyObject *self, PyObject *args, PyObject *kw) {
 PyObject *pyliburc(PyObject *self) { return Py_BuildValue("i", 0); }
 
 static PyMethodDef Module_methods[] = {
- { "liburc",            pyliburc,            METH_NOARGS },
- { "urchub_fmt",        pyurchub_fmt,        METH_VARARGS|METH_KEYWORDS},
- { "urcsign_fmt",       pyurcsign_fmt,       METH_VARARGS|METH_KEYWORDS},
+ { "liburc",            pyliburc,           METH_NOARGS },
+ { "urchub_fmt",        pyurchub_fmt,       METH_VARARGS|METH_KEYWORDS},
+ { "urcsign_fmt",       pyurcsign_fmt,      METH_VARARGS|METH_KEYWORDS},
  { "urcsign_verify",    pyurcsign_verify,    METH_VARARGS|METH_KEYWORDS},
  { "urcsecretbox_fmt",  pyurcsecretbox_fmt,  METH_VARARGS|METH_KEYWORDS},
  { "urcsecretbox_open", pyurcsecretbox_open, METH_VARARGS|METH_KEYWORDS},
+ { "urccryptobox_fmt",  pyurccryptobox_fmt,  METH_VARARGS|METH_KEYWORDS},
+ { "urccryptobox_open", pyurccryptobox_open, METH_VARARGS|METH_KEYWORDS},
  { NULL, NULL}
 };
 
@@ -137,3 +199,5 @@ void initurcsign_fmt(){ (void) Py_InitModule("urcsign_fmt", Module_methods); }
 void initurcsign_verify(){ (void) Py_InitModule("urcsign_verify", Module_methods); }
 void initurcsecretbox_fmt(){ (void) Py_InitModule("urcsecretbox_fmt", Module_methods); }
 void initurcsecretbox_open(){ (void) Py_InitModule("urcsecretbox_open", Module_methods); }
+void initurccryptobox_fmt(){ (void) Py_InitModule("urccryptobox_fmt", Module_methods); }
+void initurccryptobox_open(){ (void) Py_InitModule("urccryptobox_open", Module_methods); }
--- src/liburc.h
+++ src/liburc.h
@@ -1,5 +1,3 @@
-#include <nacl/crypto_hash_sha512.h>
-#include <nacl/crypto_hash_sha256.h>
 #include <nacl/crypto_secretbox.h>
 #include <nacl/crypto_sign.h>
 #include <nacl/crypto_box.h>
@@ -20,12 +18,11 @@ exit(255);
 exit(255);
 #endif
 
-#define URC_MTU 2+12+4+8+1024
-#define URC_MTU_MASK 1023
-#define IRC_MTU_MASK 511
+#define URC_MTU 1024
+#define IRC_MTU 512
 
 int setlen(unsigned char *b, int blen) {
- if (blen > URC_MTU_MASK) return -1;
+ if (blen > URC_MTU) return -1;
  b[0] = blen / 256;
  b[1] = blen % 256;
  return 0;
@@ -63,7 +60,7 @@ void taia96n(unsigned char *ts) {
 }
 
 int urchub_fmt(unsigned char *p, unsigned char *b, int blen) {
- if (blen > IRC_MTU_MASK) return -1;
+ if (blen > IRC_MTU) return -1;
  if (setlen(p,blen) == -1) return -1;
  taia96n(p+2);
  p[12]=0;
@@ -76,7 +73,7 @@ int urchub_fmt(unsigned char *p, unsigned char *b, int blen) {
 }
 
 int urcsign_fmt(unsigned char *p, unsigned char *b, int blen, unsigned char *sk) {
- if (blen > IRC_MTU_MASK) return -1;
+ if (blen > IRC_MTU) return -1;
  unsigned char sm[2+12+4+8+1024+64];
  unsigned long long smlen;
  if (setlen(p,blen+64) == -1) return -1;
@@ -94,7 +91,7 @@ int urcsign_fmt(unsigned char *p, unsigned char *b, int blen, unsigned char *sk)
 }
 
 int urcsign_verify(unsigned char *p, int plen, unsigned char *pk) {
- if (plen > URC_MTU_MASK) return -1;
+ if (plen > URC_MTU) return -1;
  unsigned char sm[32+2+12+4+8+1024+32];
  unsigned char m[32+2+12+4+8+1024+32];
  unsigned long long mlen;
@@ -105,7 +102,7 @@ int urcsign_verify(unsigned char *p, int plen, unsigned char *pk) {
 }
 
 int urcsecretbox_fmt(unsigned char *p, unsigned char *b, int blen, unsigned char *sk) {
- if (blen > IRC_MTU_MASK) return -1;
+ if (blen > IRC_MTU) return -1;
  unsigned char m[1024*2];
  unsigned char c[1024*2];
  bzero(m,32); /* http://nacl.cr.yp.to/secretbox.html */
@@ -131,6 +128,37 @@ int urcsecretbox_open(unsigned char *b, unsigned char *p, int plen, unsigned cha
  bzero(c,16);
  memmove(c+16,p+2+12+4+8,-2-12-4-8+plen);
  if (crypto_secretbox_open(m,c,16-2-12-4-8+plen,(const unsigned char *)p+2,(const unsigned char *)sk) == -1) return -1;
+ memmove(b,m+32,-2-4-8+plen-16);
+ return 0;
+}
+
+int urccryptobox_fmt(unsigned char *p, unsigned char *b, int blen, unsigned char *pk, unsigned char *sk) {
+ if (blen > IRC_MTU) return -1;
+ unsigned char m[1024*2];
+ unsigned char c[1024*2];
+ bzero(m,32); /* http://nacl.cr.yp.to/secretbox.html */
+ bzero(c,16);
+ if (setlen(p,blen+16) == -1) return -1;
+ taia96n(p+2);
+ p[12]=4;
+ p[13]=0;
+ p[14]=0;
+ p[15]=0;
+ randombytes(p+2+12+4,8);
+ memmove(m+32,b,blen);
+ if (crypto_box(c,m,32+blen,(const unsigned char *)p+2,(const unsigned char *)pk,(const unsigned char *)sk) == -1) return -1;
+ memmove(p+2+12+4+8,c+16,blen+16);
+ return 0;
+}
+
+int urccryptobox_open(unsigned char *b, unsigned char *p, int plen, unsigned char *pk, unsigned char *sk) {
+ if (plen > URC_MTU) return -1;
+ unsigned char m[1024*2];
+ unsigned char c[1024*2];
+ bzero(m,32); /* http://nacl.cr.yp.to/secretbox.html */
+ bzero(c,16);
+ memmove(c+16,p+2+12+4+8,-2-12-4-8+plen);
+ if (crypto_box_open(m,c,16-2-12-4-8+plen,(const unsigned char *)p+2,(const unsigned char *)pk,(const unsigned char *)sk) == -1) return -1;
  memmove(b,m+32,-2-4-8+plen-16);
  return 0;
 }
